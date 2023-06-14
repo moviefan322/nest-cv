7bf9c8d4311d0bad88d2ebf24fcbe0da8a829586
moviefan322 <philman202@yahoo.com>
2023-06-14 15:17:01 -0400

    handles errors with exception throwing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -22,12 +23,16 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 
   @Get()
-  findAllUsers(@Query('email') email: string) {
+  async findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
 
