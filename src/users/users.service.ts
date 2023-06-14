1e873ffa1256ade83810a22186e65a756d330b43
moviefan322 <philman202@yahoo.com>
2023-06-14 14:47:10 -0400

    adds logic in service to update user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,7 +21,15 @@ export class UsersService {
     return this.repo.find({ where: { email } });
   }
 
-  update(id: number) {}
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
 
   remove(id: number) {}
 }
