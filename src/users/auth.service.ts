be32248dc88aaab2da2234fba2539aed1279df7d
moviefan322 <philman202@yahoo.com>
2023-06-14 18:18:50 -0400

    manually hashes and salts password with scrypt and randombytes

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -11,8 +15,16 @@ export class AuthService {
     if (users.length) {
       throw new BadRequestException('email in use');
     }
-    
+
     // Hash the user's password
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
+
+    // Hash the password along with our new salt
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    //Join the hashed password and salt together
+    const result = salt + '.' + hash.toString('hex');
 
     // Create a new user and save it
 
