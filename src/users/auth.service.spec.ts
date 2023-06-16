aeed573ee5d61066869aa3a1b6dbb73989d9d04f
moviefan322 <philman202@yahoo.com>
2023-06-16 17:09:50 -0400

    tests for signin with unregistered email

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -47,5 +47,11 @@ describe('AuthService', () => {
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     );
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
   });
 });
