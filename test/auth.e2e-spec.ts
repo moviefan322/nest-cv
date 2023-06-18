d933b4264a6892ff5288a4fef05a812d6d8d6149
moviefan322 <philman202@yahoo.com>
2023-06-17 23:53:58 -0400

    adds test for registration and /getme'

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -31,4 +31,22 @@ describe('Authentication System', () => {
         expect(res.body.password).not.toBeDefined();
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'pooponmybutt@butt.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
