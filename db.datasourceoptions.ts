5ce560f3eb1d98e23088dd3202c9100b9b81662e
moviefan322 <philman202@yahoo.com>
2023-06-21 13:52:44 -0400

    preproduction commit

--- db.datasourceoptions.ts
+++ db.datasourceoptions.ts
@@ -20,6 +20,15 @@ switch (process.env.NODE_ENV) {
     });
     break;
   case 'production':
+    Object.assign(_DBOptions, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
     break;
   default:
     throw new Error('unknown environment');
