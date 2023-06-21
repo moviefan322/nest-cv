d681620f849d67b862fe64c1f2bf8cb94ec44ba9
moviefan322 <philman202@yahoo.com>
2023-06-21 13:41:19 -0400

    resolves db config bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DBOptions } from '../db.datasourceoptions';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
 
 @Module({
@@ -17,15 +17,27 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    // TypeOrmModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        const dbOptions: TypeOrmModuleOptions = {
+          // retryAttempts: 10,
+          // retryDelay: 3000,
+          // autoLoadEntities: false
+        };
+
+        Object.assign(dbOptions, DBOptions);
+
+        return dbOptions;
+      },
+    }),
     UsersModule,
     ReportsModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [
